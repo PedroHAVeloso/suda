c6b7f3f5c0e0630e210c307a9f18ca23698ea64e
Pedro Veloso <contatopedrohalves@gmail.com>
2023-10-18 19:03:33 -0300

    feat(web): get all zombies

--- website/lib/data/get_all_zombies.dart
+++ website/lib/data/get_all_zombies.dart
@@ -1,1 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:suda_website/data/api_url.dart';
+import 'package:suda_website/models/zombie_model.dart';
+
+Future<List<ZombieModel>> getAllZombies() async {
+  final response = await http.get(
+    Uri.parse('$apiUrl/zombies'),
+  );
+
+  if (response.statusCode != 200) throw Exception();
+
+  final zombiesJson = jsonDecode(response.body) as List;
+  final zombiesList = <ZombieModel>[];
+
+  for (final zombie in zombiesJson) {
+    zombiesList.add(
+      ZombieModel.fromJson(zombie as Map<String, dynamic>),
+    );
+  }
+
+  return zombiesList;
+}
